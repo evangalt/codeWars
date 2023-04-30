8e18e86d887ddc62d357ec64876484c0c9bc9e80
Evan Galt <evan.galt@gmail.com>
2023-04-29 22:29:58 -0500

    add sum of difference between integers

--- js/main.js
+++ js/main.js
@@ -149,6 +149,23 @@ function DNAStrand(dna){
   
   console.log(DNAStrand('AACGTCA'));
 
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+function getSum(a, b) {
+    let sum = 0;
+    if (a === b) {
+      return a;
+    } else if (a < b) {
+      for (let i = a; i <= b; i++) {
+        sum += i;
+      } return sum;
+    } else if (b < a) {
+      for (let i = b; i <= a; i++) {
+        sum += i;
+      } return sum;
+    }
+}
+
 // alternative cleaner example using JS Sets, which filter down to unique values
 function isIsogram(str){
 	return new Set(str.toUpperCase()).size == str.length;
