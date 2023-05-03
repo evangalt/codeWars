fcf7314c5d10067c5ae15a23d9fbc7afb7dfebd6
Evan Galt <evan.galt@gmail.com>
2023-05-03 09:57:56 -0500

    golf club membership categorization

--- js/main.js
+++ js/main.js
@@ -191,3 +191,30 @@ function maskify(cc) {
       arr[i] = '#';
     } return arr.join('');
   }
+
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+// Input
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+// Output
+// Output will consist of a list of string values stating whether the respective member is to be placed in the senior or open category.
+
+  function openOrSenior(data){
+    let membershipArray = [];
+    // loop through array
+    for (let i = 0; i < data.length; i++) {
+      let arrayElement = data[i];
+      // assign first value of each element to age
+      let age = arrayElement[0];
+      // assign second value of each element to handicap
+      let handicap = arrayElement[1];
+      if (age >= 55 && handicap > 7) {
+        membershipArray.push('Senior');
+      } else membershipArray.push('Open');
+    }
+    // return list of string values for "Open" or "Senior"
+    return membershipArray;
+  }
