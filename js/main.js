39226b0a11a64643fb9c5d475c32af33dc3670e2
Evan Galt <evan.galt@gmail.com>
2023-05-05 22:01:14 -0500

    population increase function

--- js/main.js
+++ js/main.js
@@ -240,3 +240,20 @@ const binaryArrayToNumber = arr => {
     });
     return total;
 };
+
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+function nbYear(p0, percent, aug, p) {
+    let population = p0;
+    let convertedPercent = percent/100;
+    let populationIncrease = population + (population * convertedPercent) + aug;
+    let numYears = 0;
+  
+    while (population < p) {
+      population = Math.floor(populationIncrease);
+      populationIncrease = population + (population * convertedPercent) + aug;
+      numYears++;
+    }
+    
+    return numYears;
+  }
