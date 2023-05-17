f2ccd618850db644094d147b8a9b1493786837cf
Evan Galt <evan.galt@gmail.com>
2023-05-16 22:46:03 -0500

    sum two numbers and convert the sum to binary

--- js/main.js
+++ js/main.js
@@ -234,8 +234,8 @@ const binaryArrayToNumber = arr => {
     // for every element, if el = 1, el = 2^index
     reversedArray.forEach((num, index) => {
         if (num === 1) {
-        let binaryNum = Math.pow(2, index);
-        total += binaryNum;
+          let binaryNum = Math.pow(2, index);
+          total += binaryNum;
         };
     });
     return total;
@@ -313,4 +313,18 @@ function findOdd(arr) {
       return parseInt(num);
     }
   }
+}
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+// The binary number returned should be a string.
+function addBinary(a, b) {
+  let sum = a + b;
+  let binary = '';
+
+  while (sum > 0) {
+    binary = (sum % 2) + binary;
+    sum = Math.floor(sum / 2);
+  }
+
+  return binary || '0';
 }
