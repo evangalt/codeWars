eeddadfa9f80cc1346bc7487265305a59332e81c
Evan Galt <evan.galt@gmail.com>
2023-06-07 21:08:43 -0500

    wording change

--- js/main.js
+++ js/main.js
@@ -314,7 +314,7 @@ function findOdd(arr) {
     }
   }
 }
-// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+// Implement a function that adds two numbers together and returns their sum in binary. 
 // The binary number returned should be a string.
 function addBinary(a, b) {
   let sum = a + b;
