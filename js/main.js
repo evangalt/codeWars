ee9b44ec3e7819f647fb7ac48342e532c8527a72
Evan Galt <evan.galt@gmail.com>
2023-06-11 22:07:13 -0500

    class grade function

--- js/main.js
+++ js/main.js
@@ -378,3 +378,16 @@ function oddOrEven(array) {
     }
   }
 };
+// get class grade
+function getGrade (s1, s2, s3) {
+  const average = (s1 + s2 + s3)/3;
+  if (average >= 90) {
+    return 'A';
+  } else if (average >= 80) {
+    return 'B';
+  } else if (average >= 70) {
+    return 'C';
+  } else if (average >= 60) {
+    return 'D';
+  } else return 'F';
+}
